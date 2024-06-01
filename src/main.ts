cfc52df9a4c029e7975ea068946d1527a3f293df
dastbal <davidxsteven@gmail.com>
2024-06-01 14:15:13 -0500

    adding commets

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   await app.listen(envs.port);
   logger.log(`Gateway running on port ${envs.port}`)
   logger.log(`Gateway running on port ${process.env.NODE_ENV}`)
-  logger.log('DAVID DD')
+  logger.log('Hello  world')
 
 }
 bootstrap();
